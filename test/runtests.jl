e8ad69c4d7d3a1b83ad11894324ec08c609be20f
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-02-19 09:34:25 +0100

    try tests Pt.3

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,27 +7,26 @@ Pkg.add("Random")
 using Random
 
 @testset "LearningMachine.jl" begin
-    # Example: Generate a dataset with 100 samples
-    num_samples = 100
-    features, labels = generate_classification_dataset(num_samples)
 
+    # Example: Generate a dataset with 100 samples
+    num_samples = 100    
     Random.seed!(42)
 
     # Generate random features (two-dimensional)
-    features = randn(num_samples, 2)
+    X = randn(num_samples, 2)
 
     # Generate corresponding labels (0 or 1)
-    response = rand([0, 1], num_samples)
+    y = rand([0, 1], num_samples)
 
     # Display the first few samples
     println("Features:")
-    println(features[1:5, :])
+    println(X[1:5, :])
     println("\nLabels:")
-    println(labels[1:5])
+    println(y[1:5])
 
     obj = LearningMachine.BaseRegressor()
-    obj["fit"](X=features[1:80, :], y=labels[1:80])
-    println(obj["predict"](X=features[81:100, :]))
+    obj["fit"](X=X[1:80, :], y=y[1:80])
+    println(obj["predict"](X=X[81:100, :]))
     @test 1 == 1
     
 end
